ff3438a25e9234f66681cb36857629855c5016ae
Bryce <brycekbargar@gmail.com>
2015-06-26 07:27:40 -0500

    Test the TrainDecision component

---
+++ test/components/TrainDecision.jsx
@@ -0,0 +1,44 @@
+import React from 'react/addons';
+import { should } from 'chai';
+
+import TrainDecision from '../../src/components/TrainDecision'
+import Train from '../../src/domain/Train'
+
+let { TestUtils } = React.addons;
+should();
+
+describe('TrainDecision component', function(){
+  before('render and locate element', function() {
+
+    var renderFor = (train) => {
+      var renderedComponent = TestUtils.renderIntoDocument(<TrainDecision train={train} />);
+      var inputComponent = TestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'div');
+
+      return inputComponent.getDOMNode();
+    }
+
+    this.trainDecisions = {
+      greenLine: renderFor(new Train('green')),
+      pinkLine: renderFor(new Train('pink')),
+      undefinedLine: renderFor(new Train()),
+      nullLine: renderFor(new Train(null)),
+      nullTrain: renderFor(null)
+    };
+  });
+
+  it('Renders the Green Line as green', function() {
+    this.trainDecisions.greenLine.className.should.equal('green');
+  });
+  it('Renders the Pink Line as pink', function() {
+    this.trainDecisions.pinkLine.className.should.equal('pink');
+  });
+  it('Renders an undefined Line as loading', function() {
+    this.trainDecisions.undefinedLine.className.should.equal('loading');
+  });
+  it('Renders a null Line as loading', function() {
+    this.trainDecisions.nullLine.className.should.equal('loading');
+  });
+  it('Renders a null Train as loading', function() {
+    this.trainDecisions.nullTrain.className.should.equal('loading');
+  });
+});
