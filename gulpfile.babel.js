d33150431e7c8cc3286738dc6c8fe734802cb1e8
Bryce <brycekbargar@gmail.com>
2015-07-12 17:34:23 -0400

    Actually remove the dependency...

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 import gulp from 'gulp';
 import sequence from 'run-sequence';
 import clean from 'del';
-import tsc from 'tsproject';
 import shell from 'gulp-shell';
 
 gulp.task('default', cb => sequence(
