8da7286ac12923ff106a29a1746265b53a66feae
Bryce <brycekbargar@gmail.com>
2015-07-12 16:10:11 -0400

    Added clean command to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,15 @@
 import gulp from 'gulp';
+import sequence from 'run-sequence';
+import clean from 'del';
 
+gulp.task('default', cb => sequence(
+  'clean',
+  cb));
 
-gulp.task('default');
+gulp.task('clean', cb => sequence(
+  [ 'clean::src',
+    'clean::test' ],
+  cb));
+
+gulp.task('clean::src', cb => clean(['www/**/*.js', 'www/**/*.js.map'], cb));
+gulp.task('clean::test', cb => clean(['test/**/*.js', 'www/**/*.js.map'], cb));
