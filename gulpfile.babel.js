b248a96c80d24b2d8423d79bf892bb462827ebbe
Bryce <brycekbargar@gmail.com>
2015-07-12 17:24:53 -0400

    Trying to fix travis with capitalization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp';
 import sequence from 'run-sequence';
 import clean from 'del';
-import tsc from 'tsproject';
+import tsc from 'TsProject';
 import shell from 'gulp-shell';
 
 gulp.task('default', cb => sequence(
